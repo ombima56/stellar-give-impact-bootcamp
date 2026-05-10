fcc5979ae18b2b4a0f59f9cc6fa2fedc7ce8459f
ombima56 <ombimahillary6@gmail.com>
2026-05-10 13:57:57 +0300

    Add main function and display functions for first and last name

---
+++ rust/week1/task_1b/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    first_name("Hillary");
+    last_name("Ombima");
+}
+
+// Use a function to display your first name
+// Use a function to display your last name
+// Use the println macro to display messages to the terminal
+
+fn first_name(name: &str) {
+    println!("First name: {}", name);
+}
+
+fn last_name(name: &str) {
+    println!("Last name: {}", name);
+}
