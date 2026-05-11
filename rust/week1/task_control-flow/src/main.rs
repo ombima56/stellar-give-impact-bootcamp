b99f309fcafa785207d6152ede978d836894ff11
ombima56 <ombimahillary6@gmail.com>
2026-05-11 22:57:28 +0300

    feat: implement main function with conditional branches for number comparison

---
+++ rust/week1/task_control-flow/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Change this value to test all three branches
+    let number = 7;
+
+    if number > 5 {
+        println!("greater than 5");
+    } else if number < 5 {
+        println!("less than 5");
+    } else {
+        println!("=5");
+    }
+}
